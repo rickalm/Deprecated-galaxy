90c20f227c5a13b868e4bd37d8a2d3e18ce5ebc1
Rick Alm <rick.alm@uptake.com>
2016-08-09 17:22:31 -0500

    patch to merge_to_alpha to support 2 repos

--- merge_to_alpha.sh
+++ merge_to_alpha.sh
@@ -1,8 +1,9 @@
 curbranch=$(git symbolic-ref --short HEAD)
 
+git push github ${curbranch}
 git checkout alpha-2.x
 git merge -m "merge ${curbranch}" ${curbranch}
 bash scripts/build.sh
 git add repo/meta
 git commit -m 'merge to alpha'
-git push; git checkout ${curbranch}
+git push github alpha-2.x; git checkout ${curbranch}
